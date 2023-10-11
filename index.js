a3a25922579470d8b972f83fe81905d1b4459406
vcj12 <jcvsignup@gmail.com>
2023-10-11 00:56:30 -0500

    beginning basic GET call

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+const http = require('http');
+
+// Create the server
+const server = http.createServer((req, res) => {
+	
+	// Check that we have found it 
+  if (req.method === 'GET' && req.url.startsWith('/api/population/state/')) {
+    const parts = req.url.split('/');
+	// Split the url into a array then store the stats, city dataso we can work with it
+    const stateIndex = parts.indexOf('state') + 1;
+    const cityIndex = parts.indexOf('city') + 1;
+	// We are all good display status: 200
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(parts));
+  } else {
+	// Something went wrong
+    res.statusCode = 404; // Not Found
+    res.end('Endpoint not found');
+  }
+});
+
+//Requested port
+const port = 5555;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
